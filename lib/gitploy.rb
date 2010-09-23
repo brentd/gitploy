34c17da0338739ef19084edff5194f69311e6438
rspeicher <rspeicher@gmail.com>
2010-09-23 17:39:26 -0400

    Add --pretend option
    Lets us test remote command execution without actually establishing a remote connection. Also it's just a useful option to have.

--- lib/gitploy.rb
+++ lib/gitploy.rb
@@ -41,7 +41,7 @@ module Gitploy
   end
 
   def deploy
-    yield if action.nil? || action == 'deploy'
+    yield unless action == 'setup'
   end
 
   def setup
@@ -81,12 +81,13 @@ module Gitploy
       print_bar(100, title)
       puts "> #{cmd}"
       puts
-      Kernel.system(cmd)
+      Kernel.system(cmd) unless pretend?
       print_bar(100)
     end
 
     def print_bar(width, title=nil)
       if title
+        title += " (pretend)" if pretend?
         half_width = (width / 2) - (title.length / 2) - 2
         left_bar  = '=' * half_width
         right_bar = '=' * (title.length % 2 == 0 ? half_width : half_width - 1) # TODO: lame.
@@ -112,5 +113,10 @@ module Gitploy
       else
         'master'
       end
+    end
+
+    def pretend?
+      pretend = %w(-p --pretend)
+      ARGV.any? { |v| pretend.include? v }
     end
 end
