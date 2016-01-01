ea691aaf570a9260697fe9c870785d5b55b4e145
Mike Glenn <mike.glenn@rammount.com>
2016-01-01 10:48:57 -0500

    fix rspec depercation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 require 'rspec'
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = :should
+  end
+end
